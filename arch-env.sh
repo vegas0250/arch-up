513f3f1d211f20cdee354955b50b5c25de1ec8a7
vegas0250 <vegas0250@gmail.com>
2015-06-20 00:17:28 +0500

    Rev

---
+++ arch-env.sh
@@ -0,0 +1,27 @@
+
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/g' /etc/locale.gen
+
+locale-gen
+
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+
+echo 'KEYMAP=ru\nFONT=cyr-sun16' > /etc/vconsole.conf
+
+ln -s /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime
+hwclock --systohc --utc
+
+echo delta > /etc/hostname
+
+systemctl enable dhcpcd@enp0s3.service
+
+mkinitcpio -p linux
+
+toor | passwd
+
+pacman -S grub
+grub-install --target=i386-pc --recheck /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+umount -R /mnt
