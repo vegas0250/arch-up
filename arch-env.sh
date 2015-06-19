b8eefe0c8f8719887f010b4549ec06243dbfbd5e
vegas0250 <vegas0250@gmail.com>
2015-06-20 00:41:06 +0500

    Rev

--- arch-env.sh
+++ arch-env.sh
@@ -7,21 +7,21 @@ locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 export LANG=en_US.UTF-8
 
-echo "KEYMAP=ru\nFONT=cyr-sun16" > /etc/vconsole.conf
+echo -e "KEYMAP=ru\nFONT=cyr-sun16" > /etc/vconsole.conf
 
 ln -s /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime
 hwclock --systohc --utc
 
 echo delta > /etc/hostname
+echo -e "#<ip-address> <hostname.domain.org> <hostname>\n127.0.0.1 localhost.localdomain localhost delta\n::1   localhost.localdomain localhost delta" > /etc/hosts
 
 systemctl enable dhcpcd@enp0s3.service
 
 mkinitcpio -p linux
 
-toor | passwd
-
 pacman -S grub
 grub-install --target=i386-pc --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
+passwd
 exit
 umount -R /mnt
